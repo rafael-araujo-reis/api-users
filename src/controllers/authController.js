1f981d26dae50e10b5d3ff4ee6cb211e9238a224
Rafael Reis <rafa.araujoreis@gmail.com>
2023-02-21 09:05:49 -0300

    created new router authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const router = express.Router();
 
@@ -17,6 +18,21 @@ router.post("/register", async (req, res) => {
   } catch (error) {
     return res.status(400).send({ error: "Registration failed" });
   }
+});
+
+router.post("/authenticate", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(404).send({ error: "User not found" });
+  }
+  if (!(await bcrypt.compare(password, user.password))) {
+    return res.status(400).send({ error: "Invalid password" });
+  }
+
+  res.send({ user });
 });
 
 module.exports = (app) => app.use("/auth", router);
