25beaba55b0e0d8c9fa335b53cdba0e0beb66247
Rafael Reis <rafa.araujoreis@gmail.com>
2023-02-21 09:18:34 -0300

    add jwt generate

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth");
 const User = require("../models/User");
 const router = express.Router();
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
 
 router.post("/register", async (req, res) => {
   const { email } = req.body;
@@ -14,7 +22,7 @@ router.post("/register", async (req, res) => {
     const user = await User.create(req.body);
     user.password = undefined;
 
-    return res.send({ user });
+    return res.send({ user, token: generateToken({ id: user.id }) });
   } catch (error) {
     return res.status(400).send({ error: "Registration failed" });
   }
@@ -34,7 +42,7 @@ router.post("/authenticate", async (req, res) => {
 
   user.password = undefined;
 
-  res.send({ user });
+  res.send({ user, token: generateToken({ id: user.id }) });
 });
 
 module.exports = (app) => app.use("/auth", router);
