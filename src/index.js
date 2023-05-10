285d6599b3f949d8d7ba478a62dcd54a1c9d577b
Rafael Reis <rafa.araujoreis@gmail.com>
2023-05-10 08:50:22 -0300

    add new script and port

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require("./app/controllers/index")(app);
 
-app.listen(3200);
+app.listen(port, () => {
+  console.info("App listening on port", port);
+});
